c6075b334a9327b1e4cd0947fa4297ce15c21350
temp <temp@STUD.UNV.SU>
2018-09-22 16:06:07 +0300

    22.09.2018 16:06:07,63
    Signed-off-by: temp <temp@STUD.UNV.SU>

---
+++ push at the end.bat
@@ -0,0 +1,10 @@
+git add *.cpp 
+git add *.sln 
+git add *.vcxproj 
+git add *.vcxproj.filters 
+git add *.h 
+git add *.bat 
+git add *.user 
+git commit -sm "%DATE% %TIME%" 
+git push steper2000 master 
+pause
