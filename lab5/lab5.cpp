5bde3465f79fda70834d041af4118ca5b2184ce0
Окунев Степан Олегович <s.o.okunev@stud.unv.su>
2018-10-06 16:03:30 +0300

    06.10.2018 16:03:30,08
    Signed-off-by: Окунев Степан Олегович <s.o.okunev@stud.unv.su>

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -32,21 +32,25 @@ class user_data
 private:
 	char first_name[255];
 	char second_name[255];
-	char str_passpot[10];
+	char str_passport[10];
 	char str_birthday[10];
 	public:
+		user_data( char p_passport[10] ,char set_birthday[10]);//конструктор
+		~user_data();//деструктор
+		
+		user_data();
 		/*bool set_first_name(char p_first_name[255])
 		{
 			for(int i=0; i<255; i++)
 			{
-				if (first_name[i] < 128 || first_name[i] <176 || first_name[i] > 241 )
+				if (first_name[i]  128 || first_name[i] <176 || first_name[i] > 241 )
 				{
 					return false;
 				}
 			}
 
-			
-		*/
+			*/
+		
 		
 		bool set_birthday(char p_birthday[10])
 	{
@@ -59,30 +63,50 @@ private:
 			
 	}
 	
-		bool set_birthday(char p_passport[10])
+		bool set_passport(char p_passport[10])
 	{
-		return false;
+			int s = 0;
+				while ( false || s<11)
+
+			{
+				if (str_passport[s] > 47 && str_passport[s] < 58)
+				{
+					return true;
+					s++;
+				}
+				else return false ;
+			}
 	};
+
 };
-	
-	
-	int main()
+
+user_data::user_data(char p_first_name[255], char p_birthday[10])
 {
-	wehicle car;
-	car.speed = 123;
+	if (set_birthday = p_birthday && set_passport = p_passport)cout << "1\n";
+	else cout<<"0\n"
+}
+	
+int main()
+{
+	
+		
+		user_data  user1(a, b);
+		{
+			if 
+		}
+	 
 
 
 
-	//применение шифра цезаря для строчных латинских букв со сдвигом вправо на 3 
+
+//применение шифра цезаря для строчных латинских букв со сдвигом вправо на 3 
 	char string[6]; // вводимая строка из 5 букв
 	cin >> string;
 	shifr_cez(string);
 	cout << string;
-/*Классы
 
-
-
-
+	/*Классы
+	
 свойство(property)-пременная в сотаве класса
 метод(method)-функ в составе класса
 инкапсуляция-фактически, об
