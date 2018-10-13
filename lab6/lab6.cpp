73a26a0ab48f6334dca23e4d2d6b4a16a247e053
Окунев Степан Олегович <s.o.okunev@stud.unv.su>
2018-10-13 16:00:04 +0300

    13.10.2018 16:00:04,27
    Signed-off-by: Окунев Степан Олегович <s.o.okunev@stud.unv.su>

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -32,12 +32,77 @@ using namespace std;
  {
  параметр1=10; // будет ошибка из-за const
  }
- 
  */
-/**/
+
+class matrix
+{
+private: 
+	int matr[10][10];
+	int rows = 10;
+	int columns = 10;
+public:
+
+	
+	void summMatrix(int *matr2)
+	{
+		for (int i = 0; i <rows; i++) 
+		{
+		 for (int j = 0; j < columns; j++)
+		 {
+			 matr2[i][j] = matr[i][j] + matr2[i][j];
+		 }
+		}
+	}
+
+	void multMatrix(int *matr2)
+	{
+		for (int i = 0; i <rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				matr2[i][j] = matr[i][j] + matr2[i][j];
+			}
+		}
+	}
+
+	
+	
+	int getElem(int row, int col)
+	{
+		if (row<rows && col<columns && row>-1 && col>-1)
+		{
+			return matr[row][col];
+		}
+		cout << "error";
+		return -1;
+	}
+
+	bool transp();
+
+	bool input();
+	void print()
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				cout << matr[i][j] << "\t";
+			}
+			cout << endl;
+		}
+	};
+};
+
+
 int main()
 {
-	int a = -15;
+	int m1;
+	
+	
+	
+	
+	
+	/*int a = -15;
 	double d = 1000.5e+03;//=1000.5 *10^3 = 1000.5 * pow(10,3) = 1000500
 	float res1;
 	long long res2;
@@ -50,13 +115,42 @@ int main()
 	cin >> ch1;
 	cout << "2nd:";
 	cin>>ch2;
-
+	
 	
 	a=atoi(ch1); // для int или long
 	d=atof(ch2);// перевод для doudle
-	//ch1 + ch2;
+	//ch1 + ch2;*/
 
-
-	return 0;
+return 0;
 }
 
+bool matrix::transp()
+{
+	int t;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			t = matr[i][j];
+			matr[i][j] = matr[j][i];
+			matr[j][i] = t;
+		}
+
+    }
+}
+
+bool matrix::input()
+{
+	cin >> rows;
+	if(rows<1 && rows>10){return false}
+	cin >> columns;
+	if (columns<1 && columns>10) { return false }
+	for(int i=0; i<rows; i++)
+	{
+		for(int j = 0; j<columns; j++)
+		{
+		 cin>>matr[i][j]
+		}
+	}
+	return true;
+}
