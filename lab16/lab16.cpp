90038020309e3ec67a1e3e32a08f8d66c72fc7ec
Steper2000 <okunev-85@bk.ru>
2018-12-27 13:15:11 +0300

    27.12.2018 13:15:11,43
    Signed-off-by: Steper2000 <okunev-85@bk.ru>

--- lab16/lab16.cpp
+++ lab16/lab16.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 //шаблон функции, которая возвращает различные типы значений
 //в зависимости от ТИПОВ входных праметров
 /*template <typename MyType>
@@ -58,17 +59,82 @@ public:
 	void push_back(TT new_elem);
 };
 */
+template<class T>
 class tree
 {
-public: int vet;
-		tree *right;
-		tree *down;
-		//void search() {};
+	struct Node
+	{
+		T item;
+		Node *son;
+		Node * bro;
+		Node(T i, Node*s = NULL, Node *b = NULL)
+		{
+			item = i;
+			son = s;
+			bro = b;
+		}
+		Node() {};
+	};
+	Node *root;//корень
+public: 
+	tree() { root = NULL; }
+	void add(const T &elem)
+	{
+		add(root, elem);
+	}
+	void f()
+	{
+		Node *temp = new Node;
+		*temp = *root;
+		std::cout << temp->item;
+		while (temp->son != NULL)
+		{
+			*temp = *temp->son;
+			std::cout << temp->item;
+			while (temp->bro != NULL)
+			{
+				*temp = *temp->bro;
+				std::cout << temp->item;
+			}
+			//*temp = *root;
+		}
+	}
+	void print()
+	{
+		Node *temp = new Node;
+		*temp = *root;
+		if (temp->son != NULL) 
+		{
+			*temp = *temp->son;
+			while (temp->bro != NULL) 
+			{
+				std::cout << temp->item;
+				*temp = *temp->bro;
+			}
+			std::cout << temp->item;
+		}
+	}
+private: void add(Node *&node, const T &item)
+	{
+	int a;
+	if (node == NULL) 
+	{
+		node = new Node(item);
+		std::cout << "Elem added. Write 0 for add bro, or 1 for add son" << std::endl;
+	}
+	else {
+		std::cin >> a;
+		if (/*item < node->item*/a > 0) {
+			add(node->son, item);
+		}
+	
+		else {
+			add(node->bro, item);
+		}
+	}
+	}
 };
-void search() 
-{
 
-};
 int main() {
 	/*	int * arrI, sizeI = 5, sizeCh = 5, sizeD = 5;
 	char * arrCh;
@@ -100,8 +166,15 @@ int main() {
 	//li.push_back(7);
 	li.print();*/
 
-
-	getchar();
+	tree<int> t1;
+	t1.add(1); //0 брат; 1 сын
+	t1.add(2);
+	t1.add(3);
+	t1.add(4);
+	t1.f();
+	//t1.print();
+	//getchar();
+	system("pause"); // Команда задержки экрана
 	return 0;
 }
 /*
